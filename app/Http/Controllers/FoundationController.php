3656dc2db00f3dd8c09ca25c276a3fd353b0552f
Maxx96 <michal.max96@gmail.com>
2020-11-11 12:25:23 +0100

    + Inicjalizacja kontrolerów, modeli i migracji

---
+++ app/Http/Controllers/FoundationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Foundation;
+use Illuminate\Http\Request;
+
+class FoundationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Foundation  $foundation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Foundation $foundation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Foundation  $foundation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Foundation $foundation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Foundation  $foundation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Foundation $foundation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Foundation  $foundation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Foundation $foundation)
+    {
+        //
+    }
+}
